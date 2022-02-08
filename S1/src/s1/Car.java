6384eb94e97865a8e6a95d48e9e3d6f0b7b43822
Kaltblotsin <Kaltblotsin@gmail.com>
2022-02-08 20:30:41 +0300

    compited

--- S1/src/s1/Car.java
+++ S1/src/s1/Car.java
@@ -1,6 +1,8 @@
 package s1;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Car {
@@ -22,6 +24,40 @@ public class Car {
         this.subdivision = subdivision;
         this.nameOfTheCarOwner = nameOfTheCarOwner;
         this.permits = permits;
+        this.file = file;
+    }
+
+    public Car(String type, String numberOfTheCar, int year, int garage, String subdivision, String nameOfTheCarOwner, ArrayList permits, File file, boolean doesCreateCarFile) throws Exception {
+        this.type = type;
+        this.numberOfTheCar = numberOfTheCar;
+        this.year = year;
+        this.garage = garage;
+        this.subdivision = subdivision;
+        this.nameOfTheCarOwner = nameOfTheCarOwner;
+        this.permits = permits;
+        this.file = file;
+        if (doesCreateCarFile) {
+            this._writeCarToFile();
+        }
+    }
+
+    public void _writeCarToFile() throws Exception {
+        try (FileWriter writer = new FileWriter(this.file, false)) {
+            writer.write(this.type + "\n" + this.numberOfTheCar + "\n" + this.year + "\n" + this.garage + "\n" + this.subdivision + "\n" + this.nameOfTheCarOwner);
+            for (int step = 0; step < this.permits.size(); step++) {
+                writer.write("\n" + this.permits.get(step));
+            }
+            writer.close();
+        } catch (IOException error) {
+            System.out.println("Ошибка:\n" + error.getMessage());
+        }
+    }
+
+    public void _deleteFile() throws Exception {
+        this.file.delete();
+    }
+
+    public void _replaceFile(File file) throws Exception {
         this.file = file;
     }
 }
--- S1/src/s1/S1.java
+++ S1/src/s1/S1.java
@@ -9,7 +9,7 @@ public class S1 {
 
     public static void _loadData(String pathToDir, LinkedList arr) throws Exception {
         for (int step = 0; true; step++) {
-            try (FileReader read = new FileReader(pathToDir + "\\Data\\id" + step + ".Car")) {
+            try (FileReader read = new FileReader(pathToDir + "Data\\id" + step + ".Car")) {
                 Scanner scan = new Scanner(read);
 
                 String type = "???";
@@ -19,7 +19,7 @@ public class S1 {
                 String subdivision = "???";
                 String nameOfTheCarOwner = "???";
                 ArrayList<String> permits = new ArrayList<>();
-                File file = new File(pathToDir + "\\Data\\id" + step + ".Car");
+                File file = new File(pathToDir + "Data\\id" + step + ".Car");
 
                 int counter = 0;
                 while (scan.hasNextLine()) {
@@ -58,7 +58,7 @@ public class S1 {
         boolean isTested = false;
 
         //create
-        try (FileReader reader = new FileReader("AppData\\Local\\S1\\PATH.txt")) {
+        try (FileReader reader = new FileReader("PATH.txt")) {
             Scanner scan = new Scanner(reader);
 
             while (scan.hasNextLine()) {
@@ -66,8 +66,8 @@ public class S1 {
             }
             reader.close();
             if ("".equals(pathToDir) || " ".equals(pathToDir) || "\n".equals(pathToDir) || " \n".equals(pathToDir)) {
-                pathToDir = "AppData\\Local\\S1";
-                try (FileWriter writer = new FileWriter("AppData\\Local\\S1\\PATH.txt", false)) {
+                pathToDir = "";
+                try (FileWriter writer = new FileWriter("PATH.txt", false)) {
                     writer.write(pathToDir);
                     writer.close();
                 }
@@ -75,16 +75,14 @@ public class S1 {
         } catch (IOException error) {
             System.out.println("Ошибка:\n" + error.getMessage());
 
-            File dir = new File("AppData\\Local\\S1");
-            dir.mkdirs();
-            try (FileWriter writer = new FileWriter("AppData\\Local\\S1\\PATH.txt", false)) {
+            try (FileWriter writer = new FileWriter("PATH.txt", false)) {
 
                 System.out.print("Введите абсолютный путь до папки с программой: ");
                 Scanner input = new Scanner(System.in);
                 pathToDir = input.nextLine();
 
                 if ("".equals(pathToDir) || " ".equals(pathToDir) || "\n".equals(pathToDir) || " \n".equals(pathToDir)) {
-                    pathToDir = "AppData\\Local\\S1";
+                    pathToDir = "";
                 }
                 writer.write(pathToDir);
                 writer.close();
@@ -93,15 +91,15 @@ public class S1 {
 
         //test
         try {
-            File dir = new File(pathToDir + "\\Data");
+            File dir = new File(pathToDir + "Data");
             dir.mkdirs();
-            try (FileWriter writer = new FileWriter(pathToDir + "\\Data\\test.Car", false)) {
+            try (FileWriter writer = new FileWriter(pathToDir + "Data\\test.Car", false)) {
                 writer.write("test");
                 writer.close();
             }
 
             String check;
-            try (FileReader reader = new FileReader(pathToDir + "\\Data\\test.Car")) {
+            try (FileReader reader = new FileReader(pathToDir + "Data\\test.Car")) {
                 Scanner scan = new Scanner(reader);
                 check = "";
                 while (scan.hasNextLine()) {
@@ -144,7 +142,7 @@ public class S1 {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
 
-    public static void _addCar(LinkedList arr) throws Exception {
+    public static void _addCar(String pathToDir, LinkedList arr) throws Exception {
         Scanner input = new Scanner(System.in);
 
         int id = arr.size();
@@ -152,33 +150,212 @@ public class S1 {
         String type = input.nextLine();
         System.out.print("numberOfTheCar(String): ");
         String numberOfTheCar = input.nextLine();
-        System.out.print("year: ");
-        int year = input.nextInt();
-        System.out.print("garage: ");
-        int garage = input.nextInt();
-        System.out.print("subdivision: ");
+        System.out.print("year(Integer): ");
+        int year = Integer.valueOf(input.nextLine());
+        System.out.print("garage(Integer): ");
+        int garage = Integer.valueOf(input.nextLine());
+        System.out.print("subdivision(String): ");
         String subdivision = input.nextLine();
-        System.out.print("nameOfTheCarOwner: ");
+        System.out.print("nameOfTheCarOwner(String): ");
         String nameOfTheCarOwner = input.nextLine();
+
         ArrayList<String> permits = new ArrayList<>();
         boolean isFirstTry = true;
         for (String oneOfPermits = "start"; !("".equals(oneOfPermits) || " ".equals(oneOfPermits) || "\n".equals(oneOfPermits) || " \n".equals(oneOfPermits));) {
             if (!(isFirstTry)) {
                 permits.add(oneOfPermits);
+            } else {
+                System.out.print("permits(Strings):\n");
             }
             oneOfPermits = input.nextLine();
             isFirstTry = false;
         }
+        File file = new File(pathToDir + "Data\\id" + id + ".Car");
+        file.createNewFile();
 
+        arr.add(new Car(type, numberOfTheCar, year, garage, subdivision, nameOfTheCarOwner, permits, file, true));
     }
 
-    public static boolean _menu(LinkedList arr) throws Exception {
+    public static void _removeLastCar(LinkedList<Car> arr) throws Exception {
+        arr.getLast()._deleteFile();
+        arr.removeLast();
+    }
+
+    public static void _removeCarByIndex(int id, LinkedList<Car> arr) throws Exception {
+        if ((arr.size() - 1) == id) {
+            _removeLastCar(arr);
+        } else if (id >= 0 && id < arr.size()) {
+            arr.set(id, arr.getLast());
+            _removeLastCar(arr);
+        } else {
+            System.out.println("Файл не удален!");
+        }
+    }
+
+    public static void _sort(String pathToDir, LinkedList<Car> arr) throws Exception {
+        boolean[] typeOfSort = _typeOfSort();
+        if (typeOfSort[0] && typeOfSort[1]) {
+            for (int stepByBubble = 0; stepByBubble < arr.size(); stepByBubble++) {
+                for (int step = 0; step < (arr.size() - 1); step++) {
+                    if (arr.get(step).year > arr.get(step + 1).year) {
+                        Car tempCar = arr.get(step);
+                        arr.set(step, arr.get(step + 1));
+                        arr.set(step + 1, tempCar);
+                        _recreateFile(pathToDir, step, arr);
+                        _recreateFile(pathToDir, step + 1, arr);
+                    }
+                }
+            }
+        } else if (typeOfSort[0] && (!typeOfSort[1])) {
+            for (int stepByBubble = 0; stepByBubble < arr.size(); stepByBubble++) {
+                for (int step = 0; step < (arr.size() - 1); step++) {
+                    if (arr.get(step).year < arr.get(step + 1).year) {
+                        Car tempCar = arr.get(step);
+                        arr.set(step, arr.get(step + 1));
+                        arr.set(step + 1, tempCar);
+                        _recreateFile(pathToDir, step, arr);
+                        _recreateFile(pathToDir, step + 1, arr);
+                    }
+                }
+            }
+        } else if ((!typeOfSort[0]) && typeOfSort[1]) {
+            for (int stepByBubble = 0; stepByBubble < arr.size(); stepByBubble++) {
+                for (int step = 0; step < (arr.size() - 1); step++) {
+                    if (arr.get(step).garage > arr.get(step + 1).garage) {
+                        Car tempCar = arr.get(step);
+                        arr.set(step, arr.get(step + 1));
+                        arr.set(step + 1, tempCar);
+                        _recreateFile(pathToDir, step, arr);
+                        _recreateFile(pathToDir, step + 1, arr);
+                    }
+                }
+            }
+        } else if ((!typeOfSort[0]) && (!typeOfSort[1])) {
+            for (int stepByBubble = 0; stepByBubble < arr.size(); stepByBubble++) {
+                for (int step = 0; step < (arr.size() - 1); step++) {
+                    if (arr.get(step).garage < arr.get(step + 1).garage) {
+                        Car tempCar = arr.get(step);
+                        arr.set(step, arr.get(step + 1));
+                        arr.set(step + 1, tempCar);
+                        _recreateFile(pathToDir, step, arr);
+                        _recreateFile(pathToDir, step + 1, arr);
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean[] _typeOfSort() {
+        boolean[] typeOfSort = new boolean[2];
+        Scanner input = new Scanner(System.in);
+        boolean isContinue = false;
+
+        do {
+            System.out.println("1: Сортировать по году.");
+            System.out.println("2: Сортировать по гаражу.");
+
+            switch (input.nextInt()) {
+                case 1 -> {
+                    System.out.println("1: Сортировать по возрастанию.");
+                    System.out.println("2: Сортировать по убыванию.");
+                    switch (input.nextInt()) {
+                        case 1 -> {
+                            typeOfSort[0] = true;
+                            typeOfSort[1] = true;
+                            isContinue = false;
+                        }
+                        case 2 -> {
+                            typeOfSort[0] = true;
+                            typeOfSort[1] = false;
+                            isContinue = false;
+                        }
+                        default -> {
+                            isContinue = true;
+                        }
+                    }
+                }
+                case 2 -> {
+                    System.out.println("1: Сортировать по возрастанию.");
+                    System.out.println("2: Сортировать по убыванию.");
+                    switch (input.nextInt()) {
+                        case 1 -> {
+                            typeOfSort[0] = false;
+                            typeOfSort[1] = true;
+                            isContinue = false;
+                        }
+                        case 2 -> {
+                            typeOfSort[0] = false;
+                            typeOfSort[1] = false;
+                            isContinue = false;
+                        }
+                        default -> {
+                            isContinue = true;
+                        }
+                    }
+                }
+                default ->
+                    isContinue = true;
+            }
+        } while (isContinue);
+
+        return typeOfSort;
+    }
+
+    public static void _showDataOfOneCar(int id, LinkedList<Car> arr) {
+        System.out.println("id: " + id);
+        System.out.println("type: " + arr.get(id).type + "\t" + "numberOfTheCar: " + arr.get(id).numberOfTheCar);
+        System.out.println("year: " + arr.get(id).year + "\t" + "garage: " + arr.get(id).garage);
+        System.out.println("subdivision: " + arr.get(id).subdivision + "\t" + "nameOfTheCarOwner: " + arr.get(id).nameOfTheCarOwner);
+        System.out.println("permits:");
+        for (int permitsStep = 0; permitsStep < arr.get(id).permits.size(); permitsStep++) {
+            System.out.println(arr.get(id).permits.get(permitsStep));
+        }
+        System.out.println("======666======");
+    }
+
+    public static void _showDataByFilter(int numberOfFilter, LinkedList<Car> arr) {
+        //numberOfFilters при 1 - по подразделению, при 2 - по имени владельца
+        Scanner input = new Scanner(System.in);
+        String checkFor;
+
+        switch (numberOfFilter) {
+            case 1 -> {
+                System.out.print("Введите название подразделения: ");
+                checkFor = input.nextLine();
+                for (int step = 0; step < arr.size(); step++) {
+                    if (checkFor.equals(arr.get(step).subdivision)) {
+                        _showDataOfOneCar(step, arr);
+                    }
+                }
+            }
+            case 2 -> {
+                System.out.print("Введите имя владельца машины: ");
+                checkFor = input.nextLine();
+                for (int step = 0; step < arr.size(); step++) {
+                    if (checkFor.equals(arr.get(step).nameOfTheCarOwner)) {
+                        _showDataOfOneCar(step, arr);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void _recreateFile(String pathToDir, int id, LinkedList<Car> arr) throws Exception {
+        arr.get(id)._deleteFile();
+        arr.get(id)._replaceFile(new File(pathToDir + "Data\\id" + id + ".Car"));
+        arr.get(id)._writeCarToFile();
+    }
+
+    public static boolean _menu(String pathToDir, LinkedList arr) throws Exception {
         Scanner input = new Scanner(System.in);
         System.out.println("1: Отобразить базу.");
         System.out.println("2: Добавить машину.");
+        System.out.println("3: Удалить машину.");
+        System.out.println("4: Сортировка машин.");
+        System.out.println("5: Фильтрация машин.");
+        System.out.println("6: Поиск машины по индентификатору.");
         System.out.println("0: Выход из программы.");
-        int choice = input.nextInt();
-        switch (choice) {
+        switch (input.nextInt()) {
             case 0 -> {
                 return false;
             }
@@ -189,7 +366,47 @@ public class S1 {
             }
             case 2 -> {
                 _cls();
-                _addCar(arr);
+                _addCar(pathToDir, arr);
+                return true;
+            }
+            case 3 -> {
+                _cls();
+                _showData(arr);
+                System.out.println("1: Удалить последнюю машину.");
+                System.out.println("2: Удалить машину по идентификатору.");
+                switch (input.nextInt()) {
+                    case 1 -> {
+                        _removeLastCar(arr);
+                    }
+                    case 2 -> {
+                        System.out.print("Введите идентификатор для удаления: ");
+                        _removeCarByIndex(input.nextInt(), arr);
+                    }
+                }
+                return true;
+            }
+            case 4 -> {
+                _cls();
+                _sort(pathToDir, arr);
+                return true;
+            }
+            case 5 -> {
+                _cls();
+                System.out.println("1: Фильтр по подразделению.");
+                System.out.println("2: Фильтр по имени владельца.");
+                int numberOfFilter = input.nextInt();
+                _showDataByFilter(numberOfFilter, arr);
+                return true;
+            }
+            case 6 -> {
+                _cls();
+                System.out.print("Введите индентификатор машины для поиска: ");
+                int search = input.nextInt();
+                try {
+                    _showDataOfOneCar(search, arr);
+                } catch (Exception error) {
+                    System.out.println("Ошибка:\n" + error.getMessage());
+                }
                 return true;
             }
             default -> {
@@ -208,6 +425,6 @@ public class S1 {
 
         _showData(arr);
 
-        while (_menu(arr));
+        while (_menu(pathToDir, arr));
     }
 }
