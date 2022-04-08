b1d9830a4904ca2ae5ed275fc6d42f52f1ef551c
Kaltblotsin <Kaltblotsin@gmail.com>
2022-04-08 13:06:59 +0300

    up

--- O1-end(adds)/O1-end(adds).cpp
+++ O1-end(adds)/O1-end(adds).cpp
@@ -1,6 +1,6 @@
 ﻿//#include "lab5add1.h" //Очередь
 //#include "lab5add2.h" //С файлами
-#include "lab5add3.h" //Из приложения
+//#include "lab5add3.h" //Из приложения
 void main() {
-	lab5add3 start;
+	lab5add1 start;
 }
